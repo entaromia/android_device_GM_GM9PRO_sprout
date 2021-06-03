b489333e7d3cb7d8bbe4a06715b5f691e51813e9
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-03 21:52:29 +0300

    GM9PRO_sprout: Import zangyapro ramdisk

--- device.mk
+++ device.mk
@@ -46,3 +46,41 @@ PRODUCT_COPY_FILES += \
 # Privapp Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    bluedut.sh \
+    bt_close.sh \
+    bt_open.sh \
+    init.bq.tp.sh \
+    init.class_main.sh \
+    init.crda.sh \
+    init.mdm.sh \
+    init.qcom.class_core.sh \
+    init.qcom.coex.sh \
+    init.qcom.crashdata.sh \
+    init.qcom.early_boot.sh \
+    init.qcom.efs.sync.sh \
+    init.qcom.post_boot.sh \
+    init.qcom.sdio.sh \
+    init.qcom.sensors.sh \
+    init.qcom.sh \
+    init.qcom.usb.sh \
+    init.qti.can.sh \
+    init.qti.fm.sh \
+    init.qti.ims.sh \
+    init.qti.qseecomd.sh \
+    iwpriv.agent.sh \
+    myftm.agent.sh \
+    qca6234-service.sh \
+    wifitest.sh \
+    wifitest_close.sh \
+    init.bq.usb.configfs.rc \
+    init.msm.usb.configfs.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.target.rc \
+    init.target_ota.rc \
+    fstab.qcom \
+    ueventd.qcom.rc
