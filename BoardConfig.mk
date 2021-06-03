dbbd457ce3a1ba1c78cedf798de73510d5c238a8
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-03 17:20:43 +0300

    GM9PRO_sprout: Add arch flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := kryo
+
 # Security patch level
 VENDOR_SECURITY_PATCH := 2020-11-01
 
