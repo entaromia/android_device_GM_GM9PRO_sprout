c555d4154463aa7f1480ea223020da78521430f5
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-04 21:30:40 +0300

    GM9PRO_sprout: Copy audio_policy_configuration.xml to /vendor/etc/
    * Instead of /vendor/etc/audio/

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ AB_OTA_PARTITIONS += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_output_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
