262628c3a4848f0abb528cae3a53d7408568b202
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-04 15:18:42 +0300

    GM9PRO_sprout: Import stock overlay
    * Power profile, vibrator and brightness pattern from GM9PRO_sprout
    * Rest from zangyapro

--- device.mk
+++ device.mk
@@ -101,6 +101,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
