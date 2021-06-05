ec112b0fb4bc92e2d663e326f973cc599ccef50f
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-05 18:13:06 +0300

    GM9PRO_sprout: Build tinyxml
    * Needed by /vendor/bin/mm-pp-dpps

--- device.mk
+++ device.mk
@@ -141,6 +141,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# Postprocessing
+PRODUCT_PACKAGES += \
+   libtinyxml
+
 # Privapp Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml
