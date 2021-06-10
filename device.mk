97b5fffff648cd00d8d78270c8ff8d08efb97058
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-10 23:14:41 +0300

    GM9PRO_sprout: Build radio hals

--- device.mk
+++ device.mk
@@ -181,7 +181,9 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
-   librmnetctl
+    android.hardware.radio@1.4 \
+    android.hardware.radio.config@1.1 \
+    librmnetctl
 
 # Ramdisk
 PRODUCT_PACKAGES += \
