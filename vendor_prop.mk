38ddcbc4eb27f9bcc2fbbe7499e44d392c81e754
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-16 21:46:55 +0300

    GM9PRO_sprout: Sort and clean-up vendor props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,97 +1,122 @@
-PRODUCT_PROPERTY_OVERRIDES +=  \
-   qemu.hw.mainkeys=0 \
-   debug.stagefright.omx_default_rank.sw-audio=1 \
-   debug.stagefright.omx_default_rank=0 \
-   vendor.vidc.enc.disable.pq=true \
-   vendor.video.disable.ubwc=1 \
-   dalvik.vm.heapgrowthlimit=256m \
-   dalvik.vm.heapstartsize=8m \
-   dalvik.vm.heapsize=512m \
-   dalvik.vm.heaptargetutilization=0.75 \
-   dalvik.vm.heapminfree=512k \
-   dalvik.vm.heapmaxfree=8m \
-   ro.hardware.nfc_nci=nqx.default \
-   vendor.hw.fm.init=0 \
-   persist.radio.multisim.config=ssss \
-   persist.vendor.qcomsysd.enabled=1 \
-   ro.hardware.vulkan=adreno \
-   ro.hardware.egl=adreno \
-   keyguard.no_require_sim=true \
-   ro.carrier=unknown \
-   ro.vendor.extension_library=libqti-perfd-client.so \
-   persist.vendor.radio.apm_sim_not_pwdn=1 \
-   persist.vendor.radio.sib16_support=1 \
-   persist.vendor.radio.custom_ecc=1 \
-   persist.vendor.radio.rat_on=combine \
-   persist.backup.ntpServer=0.pool.ntp.org \
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1 \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.adm.buffering.ms=6 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=true \
+    vendor.audio.tunnel.encode=false \
+    persist.vendor.audio.ras.enabled=true \
+    vendor.audio.offload.buffer.size.kb=64 \
+    audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    vendor.audio.offload.track.enable=true \
+    audio.deep_buffer.media=true \
+    vendor.voice.path.for.pcm.voip=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.safx.pbe.enabled=false \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio.hw.aac.encoder=true \
+    audio.sys.noisy.broadcast.delay=600 \
+    audio.sys.offload.pstimeout.secs=3 \
+    persist.vendor.audio.hifi.int_codec=true \
+    ro.af.client_heap_size_kbyte=7168 \
+    persist.vendor.audio.hw.binder.size_kbyte=1024
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.qcom.bluetooth.soc=cherokee \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
+    ro.bluetooth.a2dp_offload.supported=true \
+    persist.bluetooth.a2dp_offload.disabled=false \
+    persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac \
+    ro.vendor.bluetooth.wipower=false \
+    persist.vendor.bt.enable.splita2dp=true
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.camera.expose.aux=1 \
+    persist.vendor.camera.exif.rotation=off \
+    persist.vendor.camera.lib2d.rotation=on \
+    persist.vendor.camera.hvx.rotation=1
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
+# DPM / DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.dpm.feature=3 \
+    persist.vendor.dpm.nsrm=1 \
+    persist.vendor.dpm.loglevel=7825 \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.vulkan=adreno \
+    ro.hardware.egl=adreno \
+    ro.opengles.version=196610
+
+# Factory Reset Protection
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/bootdevice/by-name/frp
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.stagefright.omx_default_rank.sw-audio=1 \
+    debug.stagefright.omx_default_rank=0 \
+    vendor.vidc.enc.disable.pq=true \
+    vendor.video.disable.ubwc=1 \
+    vendor.mm.enable.qcom_parser=13631471
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.extension_library=libqti-perfd-client.so
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.carrier=unknown \
+    persist.radio.multisim.config=ssss \
+    persist.vendor.qcomsysd.enabled=1 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.procedure_bytes=SKIP \
+    persist.radio.vrte_logic=1 \
+    persist.radio.jbims=1 \
+    persist.radio.domain.ps=false \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.videopause.mode=1 \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.RATE_ADAPT_ENABLE=1
+
+# Sensors
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.sensors.facing=false \
+    ro.vendor.sensors.cmc=false \
+    ro.vendor.sdk.sensors.gestures=false
+
+# Shutdown Timeout
+PRODUCT_PROPERTY_OVERRIDES += \
    sys.vendor.shutdown.waittime=500 \
-   ro.build.shutdown_timeout=0 \
-   persist.vendor.radio.procedure_bytes=SKIP \
-   ro.frp.pst=/dev/block/bootdevice/by-name/frp \
-   ro.opengles.version=196610 \
-   vendor.qcom.bluetooth.soc=cherokee \
-   af.fast_track_multiplier=1 \
-   vendor.audio_hal.period_size=192 \
-   vendor.audio.adm.buffering.ms=6 \
-   ro.vendor.audio.sdk.fluencetype=fluence \
-   persist.vendor.audio.fluence.voicecall=true \
-   persist.vendor.audio.fluence.voicerec=false \
-   persist.vendor.audio.fluence.speaker=true \
-   vendor.audio.tunnel.encode=false \
-   persist.vendor.audio.ras.enabled=true \
-   vendor.audio.offload.buffer.size.kb=64 \
-   audio.offload.min.duration.secs=30 \
-   audio.offload.video=true \
-   vendor.audio.offload.track.enable=true \
-   audio.deep_buffer.media=true \
-   vendor.voice.path.for.pcm.voip=true \
-   vendor.audio.offload.multiaac.enable=true \
-   vendor.audio.dolby.ds2.enabled=false \
-   vendor.audio.dolby.ds2.hardbypass=false \
-   vendor.audio.offload.multiple.enabled=false \
-   vendor.audio.offload.passthrough=false \
-   ro.vendor.audio.sdk.ssr=false \
-   vendor.audio.offload.gapless.enabled=true \
-   vendor.audio.safx.pbe.enabled=false \
-   vendor.audio.parser.ip.buffer.size=262144 \
-   vendor.audio.flac.sw.decoder.24bit=true \
-   persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac \
-   ro.bluetooth.a2dp_offload.supported=true \
-   persist.bluetooth.a2dp_offload.disabled=false \
-   persist.bluetooth.a2dp_offload.cap=sbc-aac-aptx-aptxhd-ldac \
-   vendor.audio.use.sw.alac.decoder=true \
-   vendor.audio.use.sw.ape.decoder=true \
-   vendor.audio.hw.aac.encoder=true \
-   audio.sys.noisy.broadcast.delay=600 \
-   audio.sys.offload.pstimeout.secs=3 \
-   persist.vendor.audio.hifi.int_codec=true \
-   ro.af.client_heap_size_kbyte=7168 \
-   persist.vendor.audio.hw.binder.size_kbyte=1024 \
-   ro.vendor.sensors.facing=false \
-   ro.vendor.sensors.cmc=false \
-   ro.vendor.sdk.sensors.gestures=false \
-   rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
-   persist.vendor.qcom.bluetooth.enable.splita2dp=true \
-   persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac \
-   ro.vendor.bluetooth.wipower=false \
-   vendor.mm.enable.qcom_parser=13631471 \
-   persist.vendor.dpm.feature=3 \
-   persist.vendor.dpm.nsrm=1 \
-   persist.vendor.dpm.loglevel=7825 \
-   persist.vendor.camera.expose.aux=1 \
-   persist.vendor.usb.config=charging \
-   persist.service.adb.secure=1 \
-   drm.service.enabled=true \
-   media.mediadrmservice.enable=true \
-   persist.vendor.bt.enable.splita2dp=true \
-   persist.vendor.camera.exif.rotation=off \
-   persist.vendor.camera.lib2d.rotation=on \
-   persist.vendor.camera.hvx.rotation=1 \
-   persist.radio.calls.on.ims=true \
-   persist.radio.vrte_logic=1 \
-   persist.radio.jbims=1 \
-   persist.radio.domain.ps=false \
-   persist.radio.VT_HYBRID_ENABLE=1 \
-   persist.radio.videopause.mode=1 \
-   persist.radio.ROTATION_ENABLE=1 \
-   persist.radio.RATE_ADAPT_ENABLE=1
+   ro.build.shutdown_timeout=0
