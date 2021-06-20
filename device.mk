31f9c5afc4a8cc60c46d59a5e3918b1950ec71cb
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-20 14:28:56 +0300

    GM9PRO_sprout: Build more camera blobs from source

--- device.mk
+++ device.mk
@@ -67,7 +67,8 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.4-impl
+    android.hardware.camera.provider@2.4-impl \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Display
 PRODUCT_PACKAGES += \
