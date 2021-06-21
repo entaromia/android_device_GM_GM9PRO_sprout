d862ae9380aa520f2a2e3235965885d25b3a9b47
Tao Bao <tbao@google.com>
2021-06-21 22:46:32 +0300

    GM9PRO_sprout: AB_OTA_UPDATER is BoardConfig variable
    Bug: 130433003
    Test: TreeHugger
    Change-Id: I70c7a884df45781e8b14339edd02985ba3de02fd

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,14 @@ DEVICE_PATH := device/GM/GM9PRO_sprout
 
 BUILD_BROKEN_DUP_RULES := true
 
+# A/B
+AB_OTA_UPDATER := true
+
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
--- device.mk
+++ device.mk
@@ -11,14 +11,6 @@ $(call inherit-product, vendor/GM/GM9PRO_sprout/GM9PRO_sprout-vendor.mk)
 include $(LOCAL_PATH)/vendor_prop.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# A/B
-AB_OTA_UPDATER := true
-
-AB_OTA_PARTITIONS += \
-    system \
-    vendor \
-    boot
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
