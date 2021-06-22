e6fbb4806294e08acf76e5afd577316149491771
entaromia <30384045+entaromia@users.noreply.github.com>
2021-06-22 19:12:53 +0300

    GM9PRO_sprout: Build lineage snap

--- device.mk
+++ device.mk
@@ -60,6 +60,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.4-impl \
+    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Cryptfshw
