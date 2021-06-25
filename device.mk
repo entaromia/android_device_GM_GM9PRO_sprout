ad0428bb23148425452c0e3749f024ddf1e42679
dianlujitao <dianlujitao@lineageos.org>
2021-06-25 16:44:23 +0300

    GM9PRO_sprout: Switch to source built vendor.qti.hardware.perf@2.0.so
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    
    What's worse, the minimal CPU frequency of both clusters will be locked
    at 1113MHz on sdm660.
    
    Change-Id: I1dc96e4a51c4c81dd847c59a58ec670d89054f64

--- device.mk
+++ device.mk
@@ -221,7 +221,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.0.vendor
 
 # Privapp Permissions
 PRODUCT_COPY_FILES += \
