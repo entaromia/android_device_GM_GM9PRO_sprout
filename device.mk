21dc5d11d348bb181bfd92f237ea1969d1bafd8a
entaromia <30384045+entaromia@users.noreply.github.com>
2021-07-31 19:04:55 +0300

    GM9PRO_sprout: Setup verity
    Signed-off-by: entaromia <30384045+entaromia@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -343,6 +343,11 @@ PRODUCT_HOST_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Verity
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/c0c4000.sdhci/by-name/vendor
+$(call inherit-product, build/target/product/verity.mk)
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-service \
