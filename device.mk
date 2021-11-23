ce905494ed4e8446cee55af312bbbc5165aa783c
entaromia <entaromia@gmail.com>
2021-11-23 18:11:17 +0300

    GM9PRO_sprout: rootdir: Fold init.target_*.rc into init.target.rc
    * init.target_dap.rc also causes issues when encryption is enabled.
    
    Signed-off-by: entaromia <entaromia@gmail.com>

--- device.mk
+++ device.mk
@@ -312,8 +312,6 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.post_boot.sh \
     init.target.rc \
-    init.target_dap.rc \
-    init.target_ota.rc \
     fstab.qcom \
     init.recovery.qcom.rc \
     init.qti.ims.sh \
