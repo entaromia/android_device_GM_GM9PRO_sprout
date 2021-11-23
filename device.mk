e20c4c0864383a1085a3ee37a4ec6af76d1d37af
Bruno Martins <bgcngm@gmail.com>
2021-11-23 22:10:30 +0300

    GM9PRO_sprout: Switch to cryptfshw HIDL
    Change-Id: Id1d580607a7d73ec6738ab3aa8dcdf9e27558a33

--- device.mk
+++ device.mk
@@ -90,7 +90,8 @@ PRODUCT_COPY_FILES += \
 
 # Cryptfshw
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0.vendor
+    vendor.qti.hardware.cryptfshw@1.0.vendor \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
 # Display
 PRODUCT_PACKAGES += \
