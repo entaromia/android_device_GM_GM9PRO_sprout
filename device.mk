e266df3dc0c02cdfaeceeef854bea28b031bb9cb
Jun Wang <junwan@codeaurora.org>
2021-11-27 17:04:59 +0300

    GM9PRO_sprout: Add atrace-hal build rule
    CRs-Fixed: 2453668
    Change-Id: If995d1bf5cae68e09c156ac297fd7a5f5ca0d209

--- device.mk
+++ device.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Atrace
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
