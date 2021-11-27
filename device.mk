587324169ecef3a3559049bcd17567e0aaa669e4
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-11-27 17:18:48 +0300

    GM9PRO_sprout: Enable power-libperfmgr
    Change-Id: Ia5f8fb4a2afc7a453eb09d2092a7872fd8442bc9
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: entaromia <entaromia@gmail.com>

--- device.mk
+++ device.mk
@@ -287,9 +287,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.0.vendor
+    android.hardware.power-service.gm9pro-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
